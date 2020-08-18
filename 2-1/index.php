338a4e94a0b225211d24d18c2e11e17a929c9589
kidonaoki <69570863+kidonaoki@users.noreply.github.com>
2020-08-18 15:56:29 +0900

    課題提出
    2-1

---
+++ 2-1/index.php
@@ -0,0 +1,27 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+1-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.開発環境・検証環境・本番環境"."<br>";
+echo"開発環境とは開発者が自由に使える環境であり、基本的には自身の手元のパソコンの環境のことが多い。
+    検証環境とは別名テスト環境。開発し終わったリリース予定の機能がちゃんと動くかテストするための環境のこと。
+    本番環境とはその名の通り、サービスを提供して実運用を行う環境のこと。つまり、顧客先の所有物扱いとなるため、無断でコピーや編集を行ってはいけない。"."<br>";
+echo"2.基本設計書・詳細設定書"."<br>";
+echo"基本設計書とはお聞きしたクライアントの要件を、実際にシステムとして「見えるか」させる作業のこと。つまり、ユーザーやクライアント向けの基本設計である。
+    一方、詳細設定書とはプログラマー向けの設計書の作成が中心である。SEの仕事であり、システム内部の動作や機能の設計を行う。"."<br>";
+echo"3.Ajax"."<br>";
+echo"AjaxとはJavaScriptとXMLを使って非同期にサーバーとの間の通信を行うこと。"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" 
+alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
