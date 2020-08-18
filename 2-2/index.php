a464b303cc3f12c1a510d0d1f33d7bb5c8680c63
kidonaoki <69570863+kidonaoki@users.noreply.github.com>
2020-08-18 16:54:28 +0900

    課題提出
    2-2

---
+++ 2-2/index.php
@@ -0,0 +1,41 @@
+<?php
+// index.php
+$message = "Hello World!";
+var_dump($message);
+echo '<br>';
+
+$x = 8;
+$y = 5.55;
+echo '<br>';
+var_dump($x);
+echo '<br>';
+var_dump($y);
+echo '<br>';
+?>
+
+<?php
+$flag = true;
+echo '<br>';
+var_dump($flag);
+?>
+
+<?php
+//右辺には何も記述しない
+$emp = null;
+var_dump($emp);
+echo '<br>';
+?>
+
+<?php
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';
+var_dump($ja_words);
+echo '<br>';
+?>
+
+<?php
+echo"1.APIとはプログラム内で他のサービスと連携するためのインターフェースのこと。"."<br>";
+echo"2.SVNとはクライアントとサーバー型の集中型バージョン管理型システムのこと。"."<br>";
+?>
