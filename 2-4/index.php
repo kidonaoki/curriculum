222ffb43db8f905f0b4108b41967b4a365ef0aab
kidonaoki <69570863+kidonaoki@users.noreply.github.com>
2020-08-27 22:49:51 +0900

    課題提出
    2-4

---
+++ 2-4/index.php
@@ -0,0 +1,57 @@
+<?php
+// 代入演算子
+echo "hello" . "world";
+echo '<br>';
+$hello = "Hello";
+$world = "world!!";
+echo $hello.$world;
+echo '<br>';
+
+echo 2 + 3;
+echo '<br>';
+echo 9 - 4;
+echo '<br>';
+echo 10 * 5;
+echo '<br>';
+echo 16 * 4;
+echo '<br>';
+echo 16 % 3;
+?>
+
+<?php
+echo '<br>';
+// 単項演算子
+// $x = $x + 1
+$x = 4;
+$x++;
+echo $x;
+echo '<br>';
+// $x = $x - 1
+$x =6;
+$x--;
+echo $x;
+echo '<br>';
+?>
+
+<?php
+// $x = $x + 10;
+$x =10;
+$x += 10;
+echo $x;
+echo '<br>';
+
+// $x = $x * 15;
+$x = 15;
+$x *= 15;
+echo $x;
+echo '<br>';
+?>
+
+<?php
+echo "1.エビデンスとは証拠が一般的な意味合いとなるが、システムの開発のテスト工程等で、システムが正しく動いたかエラーが出たか等の証拠となるデータのことを指す。";
+echo '<br>';
+echo "2.バッチとはあらかじめ登録しておいた一連の処理を自動的に実行する処理方式のことで、大量のデータを順序決めし、一括処理すること。";
+echo '<br>';
+echo "3.MVCモデルとは、プログラムの中身を「モデル」、「ビュー」、「コントローラ」の三つに分類すること。";
+?>
+
