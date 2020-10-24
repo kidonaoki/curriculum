820d3e9350b881fbda203ac07e9443e853a11e8a
kidonaoki <69570863+kidonaoki@users.noreply.github.com>
2020-10-24 12:17:33 +0900

    JavaScript1-1

---
+++ 1-1/main.js
@@ -0,0 +1,19 @@
+console.log("Hello World!!");
+console.log(65536);
+console.log(10 + 5);
+console.log(10 * 5);
+console.log("Hello World!!");
+console.log("Hello " + "World!!");
+//割り算の余りの値を求めたいときは「%」を使用します。
+console.log(10 % 2);
+/*
+上記の場合、10割る2で余りは0
+そのため出力は[0]
+下記の場合、出力は[1]となります
+*/
+console.log(10 % 3);
+console.log("10 + 8");
+console.log(10 + 8);
+console.log("20割る3の余りは" + 20 % 3);
+
+window.alert("jsやらないと！");
