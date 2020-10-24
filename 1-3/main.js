6f525819eb428b21854ed20eeed722cc1a65dd0d
kidonaoki <69570863+kidonaoki@users.noreply.github.com>
2020-10-24 12:54:50 +0900

    JavaScript1-3

---
+++ 1-3/main.js
@@ -0,0 +1,29 @@
+//問１
+let a = 15;
+if (a != 10){
+    console.log(`aは10ではありません`);
+}
+
+let b = 14;
+if (b >= 10 && b <20 && b % 2 === 0){
+    console.log(`bは10以上20未満、かつ偶数である`);
+}
+
+//問2
+let x = 10;
+if(x >= 10 && x <= 20){
+    console.log(`成功です`);
+} else{
+    console.log(`失敗です`);
+}
+
+//問3
+let y = 0;
+if(y % 2 === 0){
+    console.log(`偶数です`);
+} else{
+    console.log(`奇数です`);
+}
+
+
+
