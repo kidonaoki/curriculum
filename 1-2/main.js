5bb71e509a2dc10f48a5bee3f2c534f8bf76420c
kidonaoki <69570863+kidonaoki@users.noreply.github.com>
2020-10-24 12:42:08 +0900

    JavaScript1-2

---
+++ 1-2/main.js
@@ -0,0 +1,12 @@
+let name = "hoge";
+console.log(name);
+
+name = "木戸";
+let number = 11;
+console.log(`${name}くんは${number}月生まれです`);
+
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
